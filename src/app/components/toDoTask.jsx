1eb8dac42979137c3dc3d430b4da964a556482c6
DannyCroDev <danielrajic26@outlook.com>
2024-03-19 16:06:18 +0100

    css additions

--- src/app/components/toDoTask.jsx
+++ src/app/components/toDoTask.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { IconButton, Checkbox } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Typography } from "@mui/material";
 import "../css/toDoTask.css";
 
 export default function ToDoTask({ task, handleDeleteTask }) {
   return (
     <div className="task-container">
-      <Checkbox defaultChecked={task.completed} className="completedCheckbox" />
-      <p className={task.completed ? "completed-task" : "not-completed-task"}>{task.title}</p>
-      {task.description && <p className="task-description">{task.description}</p>}
+      <div className="taskIconsDiv">
+        <Checkbox defaultChecked={task.completed} className="completedCheckbox" />
+        <Typography variant="h6" className={task.completed ? "completed-task" : "not-completed-task"}>
+          {task.title}
+        </Typography>
+      </div>
+      <div className="taskDetailsDiv">
       <IconButton aria-label="delete" onClick={() => handleDeleteTask(task.id)}>
-        <DeleteIcon className="deleteIcon"/>
-      </IconButton>
+          <DeleteIcon className="deleteIcon"/>
+        </IconButton>       
+        {task.description && (
+          <Typography variant="body2" className="task-description">
+            {task.description}
+          </Typography>
+        )}
+      </div>
     </div>
   );
 }
