aea7a6a6088f885d4380c85ae8cedd76f2daa6e7
DannyCroDev <danielrajic26@outlook.com>
2024-03-19 15:13:52 +0100

    css additions

--- src/app/components/toDoTask.jsx
+++ src/app/components/toDoTask.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { IconButton, Checkbox } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import "../css/toDoTask.css";
 
 export default function ToDoTask({ task, handleDeleteTask }) {
   return (
     <div className="task-container">
-      <Checkbox defaultChecked={task.completed} />
-      <p className={task.completed ? "completed-task" : ""}>{task.title}</p>
+      <Checkbox defaultChecked={task.completed} className="completedCheckbox" />
+      <p className={task.completed ? "completed-task" : "not-completed-task"}>{task.title}</p>
       {task.description && <p className="task-description">{task.description}</p>}
       <IconButton aria-label="delete" onClick={() => handleDeleteTask(task.id)}>
-        <DeleteIcon />
+        <DeleteIcon className="deleteIcon"/>
       </IconButton>
     </div>
   );
